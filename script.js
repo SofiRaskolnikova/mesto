de77980850464e69c6d68581f6b90dbcf7fbb7be
Sofia Raskolnikova <sofiaraskolnikova@yandex.ru>
2023-02-21 23:19:43 +0400

    Пофиксить после 1 ревью

--- script.js
+++ script.js
@@ -1,56 +1,34 @@
 let editProfileButton = document.querySelector('.profile__button-edit');
 let editPopup = document.querySelector('.popup');
 let closePopupButton = document.querySelector('.popup__button-close');
-let savePopupButton = document.querySelector('.popup__button-save');
-if (!closePopupButton) {
-    throw new Error ('No closePopupButton');
-}
-
-if (!editProfileButton) {
-    throw new Error ('No editProfileButton');
-}
-editProfileButton.addEventListener('click', openEditPopup);
+let profileUserNameElement = document.querySelector('.profile__title');
+let profileJobElement = document.querySelector('.profile__text');
+let popupInputName = document.querySelector('.popup__item_input_name');
+let popupInputJob = document.querySelector('.popup__item_input_job');
+let formElement = document.querySelector('.profile__container');
 
 function openEditPopup () {
+  popupInputName.value = profileUserNameElement.textContent;
+  popupInputJob.value = profileJobElement.textContent;
   editPopup.classList.add('popup_opened');
 };
 
-closePopupButton.addEventListener('click', closePopup);
+editProfileButton.addEventListener('click', openEditPopup);
 
 function closePopup () {
   editPopup.classList.remove('popup_opened');
 };
 
-let profileUserName = 'Жак-Ив Кусто';
-let profileUserJob = 'Исследователь океана';
-
-let profileUserNameElement = document.querySelector('.profile__title');
-profileUserNameElement.textContent = profileUserName;
-
-let profileJobElement = document.querySelector('.profile__text');
-profileJobElement.textContent = profileUserJob;
-
-let popupInputName = document.querySelector('.popup__item_input_name');
-popupInputName.value = profileUserName;
-
-let popupInputJob = document.querySelector('.popup__item_input_job');
-popupInputJob.value = profileUserJob;
-
-let formElement = document.querySelector('.profile__container');
+closePopupButton.addEventListener('click', closePopup);
 
 function editUserInfo(evt) {
   evt.preventDefault();
-  let popupInputName = document.querySelector('.popup__item_input_name').value;
-  profileUserNameElement.innerHTML = popupInputName;
-  let popupInputJob = document.querySelector('.popup__item_input_job').value;
-  profileJobElement.innerHTML = popupInputJob;
-
+  popupInputName.value;
+  popupInputJob.value;
+  profileUserNameElement.textContent = popupInputName.value;
+  profileJobElement.textContent = popupInputJob.value;
+  
   closePopup();
 };
 
-savePopupButton.addEventListener('click',editUserInfo);
 formElement.addEventListener('submit', editUserInfo);
-
-  
-
-
